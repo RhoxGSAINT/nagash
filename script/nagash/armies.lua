913704eadba756b8ebefc1edf932b6d3d6d4bb3b
Cataph <54819130+Cataph@users.noreply.github.com>
2022-02-15 18:14:46 +0100

    maybe tech effects done and stuff
    - finished vlad techs
    - finished manny techs
    - finished krell techs
    - finished neffy techs
    - finished location techs
    - replaced "warbeast" with "war beast" in locs
    - made a ram list for shambling horde more or less

--- script/nagash/armies.lua
+++ script/nagash/armies.lua
@@ -7,7 +7,11 @@ random_army_manager:new_force("nag_death")
 random_army_manager:add_mandatory_unit("nag_death", "nag_nagashi_guard", 1)
 
 --- Numbers here are "weights", which are pooled to determine chance.
-random_army_manager:add_unit("nag_death", "nag_spirit_hosts", 1)
-random_army_manager:add_unit("nag_death", "nag_bone_golems", 1)
+random_army_manager:add_unit("nag_death", "nag_spirit_hosts", 2)
+random_army_manager:add_unit("nag_death", "nag_vanilla_tmb_cav_skeleton_horsemen_0", 2)
+random_army_manager:add_unit("nag_death", "nag_vanilla_tmb_inf_skeleton_archers_0", 2)
+random_army_manager:add_unit("nag_death", "nag_vanilla_tmb_inf_skeleton_spearmen_0", 6)
+random_army_manager:add_unit("nag_death", "nag_vanilla_vmp_inf_zombie", 6)
+
 
 --- TODO armies for each ritual!
