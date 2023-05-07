37c0cd74b9ea4650570b810f8cd88dce046370e6
RhoxGSAINT <swordknight@naver.com>
2023-05-07 10:45:45 +0900

    commit before the merge
    Just some porthole and disabling vamp endgame

--- script/campaign/wh3_main_combi/mod/rhox_nagash_init.lua
+++ script/campaign/wh3_main_combi/mod/rhox_nagash_init.lua
@@ -409,14 +409,14 @@ end
 
 
 
------------------this is to remove the black pyramid end game
+-----------------this is to remove the black pyramid and vampire rise end game
 cm:add_post_first_tick_callback(
     function()
         if cm:is_new_game() and #endgame.scenarios > 0 then --there is something in the end game scenarios
             for i=1, #endgame.scenarios do
                 local value = endgame.scenarios[i]
-                if value == "endgame_pyramid_of_nagash" then
-                    table.remove(endgame.scenarios, i) --remove black pryramid
+                if value == "endgame_pyramid_of_nagash" or value == "endgame_vampires_rise" then
+                    table.remove(endgame.scenarios, i) --remove black pryramid or vampire rise
                     break
                 end
             end
