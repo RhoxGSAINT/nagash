808ed8f5ed682436036f35c0cd5a28d632a96200
RhoxGSAINT <swordknight@naver.com>
2023-10-12 13:58:20 +0900

    Follower and divinity Rite
    - New script for the followers and turned random drop column to false
    - Divinity now applies to Nagash and his enemy only

--- script/campaign/wh3_main_combi/mod/rhox_nagash_follower.lua
+++ script/campaign/wh3_main_combi/mod/rhox_nagash_follower.lua
@@ -188,8 +188,150 @@ local followers = {
 
 			end,
 		["chance"] = 10
-	}
+	},
+	{
+		["follower"] = "nag_anc_follower_black_cat",
+		["event"] = "CharacterCreated",
+		["condition"] =
+			function(context)
+                local character = context:character()
+            
+				return character:character_subtype_key() == "nag_mortarch_neferata" or character:character_subtype_key() == "nag_lahmia_neferata"
+
+			end,
+		["chance"] = 100
+	},
+	{
+		["follower"] = "nag_anc_follower_black_cat",--can use same name as a listener again. Only will be problematic if we stop the listener by function
+		["event"] = "CharacterCreated",
+		["condition"] =
+			function(context)
+                local character = context:character()
+            
+				return character:character_type_key() == "dignitary" and character:faction():name() =="mixer_nag_nagash" and character:character_subtype_key() ~= "nag_mortarch_drekla"
+
+			end,
+		["chance"] = 50
+	},
+	{
+		["follower"] = "nag_anc_follower_warlock",
+		["event"] = "CharacterCompletedBattle",
+		["condition"] =
+			function(context)
+                local character = context:character()
+				local faction = character:faction()
+                local faction_key = faction:name()
+				return character:faction():name() =="mixer_nag_nagash" and context:pending_battle():ambush_battle();
+			end,
+		["chance"] = 25
+	},
+	{
+		["follower"] = "nag_anc_follower_corpse_caravan",
+		["event"] = "CharacterCompletedBattle",
+		["condition"] =
+			function(context)
+                local character = context:character()
+				local faction = character:faction()
+                local faction_key = faction:name()
+				return character:faction():name() =="mixer_nag_nagash" and character:won_battle();
+			end,
+		["chance"] = 1
+	},
+	{
+		["follower"] = "nag_anc_follower_death_cultist",
+		["event"] = "CharacterPostBattleCaptureOption",
+		["condition"] =
+			function(context)
+                local character = context:character()
+				local faction = character:faction()
+                local faction_key = faction:name()
+				return character:faction():name() =="mixer_nag_nagash" and context:get_outcome_key() == "enslave" and cm:char_is_general_with_army(context:character());
+
+			end,
+		["chance"] = 5
+	},
+	{
+		["follower"] = "nag_anc_follower_siren",
+		["event"] = "CharacterTurnEnd",
+		["condition"] =
+			function(context)
+				local character = context:character();
+				return character:faction():name() =="mixer_nag_nagash" and character:has_region() and character:action_points_remaining_percent() > 75 and (character:region():building_exists("nag_outpost_main_port_1"));
+			end,
+		["chance"] = 6
+	},
+	{
+		["follower"] = "nag_anc_follower_poltergeist",
+		["event"] = "CharacterPostBattleCaptureOption",
+		["condition"] =
+			function(context)
+                local character = context:character()
+				local faction = character:faction()
+                local faction_key = faction:name()
+				return character:faction():name() =="mixer_nag_nagash" and context:get_outcome_key() == "release" and cm:char_is_general_with_army(context:character());
+			end,
+		["chance"] = 5
+	},
+	{
+		["follower"] = "nag_anc_follower_necromancers_apprentice",
+		["event"] = "CharacterRankUp",
+		["condition"] =
+			function(context)
+                local character = context:character();
+				if character:has_region() then
+					local region = character:region();
+					return region:owning_faction() == character:faction() and character:faction():name() =="mixer_nag_nagash";
+				end;
+			end,
+		["chance"] = 15
+	},
+	{
+		["follower"] = "nag_anc_follower_embalmer",
+		["event"] = "CharacterRankUp",
+		["condition"] =
+			function(context)
+                local character = context:character();
+				return character:turns_in_enemy_regions() > 0 and character:faction():name() =="mixer_nag_nagash";
+			end,
+		["chance"] = 8
+	},
+	{
+		["follower"] = "nag_anc_follower_powder_monkey",
+		["event"] = "CharacterCompletedBattle",
+		["condition"] =
+			function(context)
+                local character = context:character()
+				local faction = character:faction()
+                local faction_key = faction:name()
+				return character:faction():name() =="mixer_nag_nagash" and character:won_battle() and cm:char_army_has_unit(character, {"nag_vanilla_cst_inf_deck_gunners_0"});
+			end,
+		["chance"] = 10
+	},
+	{
+		["follower"] = "nag_anc_follower_cursed_necrotect",
+		["event"] = "CharacterCompletedBattle",
+		["condition"] =
+			function(context)
+                local character = context:character()
+				local faction = character:faction()
+                local faction_key = faction:name()
+				return character:faction():name() =="mixer_nag_nagash" and character:won_battle() and cm:character_won_battle_against_culture(context:character(), {"wh2_dlc11_cst_vampire_coast", "wh2_dlc09_tmb_tomb_kings", "wh_main_vmp_vampire_counts", "mixer_vmp_jade_vampires", "mixer_nag_nagash"});
+
+			end,
+		["chance"] = 25
+	},
+	{
+		["follower"] = "nag_anc_follower_the_ravenous_dead",
+		["event"] = "CharacterRankUp",
+		["condition"] =
+			function(context)
+                local character = context:character();
+				return character:faction():name() =="mixer_nag_nagash" and character:military_force():contains_mercenaries();
+			end,
+		["chance"] = 15
+	},
 };
+
 
 local function rhox_nagash_load_followers()
 	for i = 1, #followers do
