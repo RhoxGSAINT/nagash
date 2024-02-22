37ac0c94f286471ed25664e4b6c5bc20ec788852
RhoxGSAINT <swordknight@naver.com>
2024-02-23 01:33:32 +0900

    rampage changes
    1. Changed Tomb Summon unit card to normal one
    2. Added rampage attribute to Tomb summon unit
    3. Removed Tomb battle script
    4. Added rampage ability to Thrall and removed rampage attribute
    5. nag_unit_passive_marshlight_aura increased recharge time to 120 from 90, increased effect range to 60 from 30
    6. nag_azhag_abilities_reckless_whispers_iii: Now also has glorious charge, also increased the charge bonus to 32 from 24

--- script/battle/mod/nagash_blyramid_bombardment.lua
+++ script/battle/mod/nagash_blyramid_bombardment.lua
@@ -38,7 +38,7 @@ local function scan_units_for_targets(alliance_armies, enemy_armies)
 				
 				-- spawns blyramid and remembers coordinates
 				if (type_key == "nag_nagash_boss" or type_key == "nag_nagash_husk") and spawn_blyramid < 1 then  --because of uber husk, we have to put husk condition
-                    out("Rhox Nagash: I'm spawning the black Pyramid!")
+                    --out("Rhox Nagash: I'm spawning the black Pyramid!")
 					-- current_unit:position();
 					blyramid_anchor_coords = current_unit:position();
 					
@@ -47,39 +47,13 @@ local function scan_units_for_targets(alliance_armies, enemy_armies)
 					-- gb.sm:trigger_message("test");
 					army:use_special_ability("nag_blyramid_itself", blyramid_anchor_coords, d_to_r(0))
 					--army:use_special_ability("nag_blyramid_itself", blyramid_anchor_coords)
-					out("Rhox Nagash: Summoned black pyramid x, y, z is: "..blyramid_anchor_coords:get_x().."/"..blyramid_anchor_coords:get_y().."/"..blyramid_anchor_coords:get_z())
+					--out("Rhox Nagash: Summoned black pyramid x, y, z is: "..blyramid_anchor_coords:get_x().."/"..blyramid_anchor_coords:get_y().."/"..blyramid_anchor_coords:get_z())
 					spawn_blyramid = 1
-				end
-                
-				-- sends units summoned diretly at the enemy and takes control away from player
-				if (type_key == "nag_inf_skeleton_warriors_endless_tomb_summoned") then
-					
-					-- out("script nag_inf_skeleton_warriors_endless_tomb_summoned")
-					-- script_unit:new(current_army, unit_index_in_army);
-					-- local uc = army:create_unit_controller();
-					-- uc:add_units(current_unit);
-					-- unitgroup.sunits:item(i);
-					if current_unit:is_idle() then			
-						-- out("script nag_inf_skeleton_warriors_endless_tomb_summoned not mowing")			
-						endless_tomb_warrior_pos = current_unit:position();	
-						closest_unit = get_closest_standing_unit(enemy_armies, endless_tomb_warrior_pos)
-						closest_unit_pos = closest_unit:position();
-						current_unit_uc = su.uc
-
-						current_unit_uc:take_control();
-						current_unit_uc:attack_unit(closest_unit, true, true);
-						-- current_unit_uc:attack_line(endless_tomb_warrior_pos, closest_unit_pos, true)
-
-						-- testing ping
-						-- gb:add_ping_icon_on_message("test", endless_tomb_warrior_pos, 4, 1000, 8000);
-						-- gb.sm:trigger_message("test");
-						
-					end					
 				end
                 
 				-- for blyramid bombardment, deletes targeting unit and deploys 5 bombardments in that location
 				if type_key == "nag_bombardment_targeting" then
-                    out("Rhox Nagash: Black Pyramid Fire!")
+                    --out("Rhox Nagash: Black Pyramid Fire!")
 					summoned = su
 					summoned:cache_location()
 					pos = summoned:get_cached_position()
@@ -88,14 +62,14 @@ local function scan_units_for_targets(alliance_armies, enemy_armies)
 					summoned:set_enabled(false)
 					summoned:kill(true)
 					last_kill = os.clock()
-                    out("Rhox Nagash: spawn_blyramid is: "..spawn_blyramid)
+                    --out("Rhox Nagash: spawn_blyramid is: "..spawn_blyramid)
 					if spawn_blyramid == 1 then
 						-- blyramid_anchor_coords
 						-- pos
 						-- math.atan2(targetY-gunY, targetX-gunX)
 						-- calculate angle between target and blyramid
-						out("Rhox Nagash: black pyramid anchor coords x, y, z is: "..blyramid_anchor_coords:get_x().."/"..blyramid_anchor_coords:get_y().."/"..blyramid_anchor_coords:get_z())
-						out("Rhox Nagash: Summoned target's position's x, y, z is: "..pos:get_x().."/"..pos:get_y().."/"..pos:get_z())
+						--out("Rhox Nagash: black pyramid anchor coords x, y, z is: "..blyramid_anchor_coords:get_x().."/"..blyramid_anchor_coords:get_y().."/"..blyramid_anchor_coords:get_z())
+						--out("Rhox Nagash: Summoned target's position's x, y, z is: "..pos:get_x().."/"..pos:get_y().."/"..pos:get_z())
 						--y = pos:get_z()-(blyramid_anchor_coords:get_z() + 1400)
 						y = pos:get_y()-(blyramid_anchor_coords:get_y() + 1400)
 						x = pos:get_x()-(blyramid_anchor_coords:get_x() + 1400)
@@ -103,9 +77,9 @@ local function scan_units_for_targets(alliance_armies, enemy_armies)
 						original_y = pos:get_y()
 						original_z = pos:get_z()						
 						angle_radians = math.atan2(y, x)
-						out("Rhox Nagash: angle_radians: "..angle_radians)
+						--out("Rhox Nagash: angle_radians: "..angle_radians)
 						angle_degrees = math.deg(angle_radians)
-                        out("Rhox Nagash: angle_degrees: "..angle_degrees)
+                        --out("Rhox Nagash: angle_degrees: "..angle_degrees)
                         
                         y_generated = bm:get_terrain_height(original_x, original_z)
                         --y_generated = original_y
