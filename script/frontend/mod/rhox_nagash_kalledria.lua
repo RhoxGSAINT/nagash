8f58d01d4f15cea15fdec3da0ceebbda12553c95
RhoxGSAINT <swordknight@naver.com>
2024-07-01 17:50:10 +0900

    Syreen
    - Changed Syreen Kalledria had access to vanilla main units from the custom key (the vampire count one, not the Nagash one)
    -- Less maintenance
    -- Less conflict if some other mod decides to add Syreen to Vampire Count
    -- On the downside, I have to add more faction-banned purchasable effect
    - Reduced Nagash Syreen's starting cap to 0 from 4 as all Nagash capped units are like that
    - Added Nagash units without capped to the allied recruitable list. At least fewer people will ask about it. Or I hope

--- script/frontend/mod/rhox_nagash_kalledria.lua
+++ script/frontend/mod/rhox_nagash_kalledria.lua
@@ -4,7 +4,7 @@ core:add_ui_created_callback(
     
             if common.get_context_value("CcoOwnershipProductRecord", "TW_WH3_SHADOWS_OF_CHANGE", "IsOwned") then
                 mixer_enable_custom_faction("1568726704")
-                mixer_add_starting_unit_list_for_faction("mixer_vmp_wailing_conclave", {"nag_vmp_kalledria_syreen","wh_main_vmp_inf_crypt_ghouls","wh_main_vmp_cav_hexwraiths", "wh_main_vmp_inf_cairn_wraiths"})
+                mixer_add_starting_unit_list_for_faction("mixer_vmp_wailing_conclave", {"wh2_dlc11_cst_inf_syreens","wh_main_vmp_inf_crypt_ghouls","wh_main_vmp_cav_hexwraiths", "wh_main_vmp_inf_cairn_wraiths"})
                 
                 mixer_change_lord_name("1568726704", "nag_vmp_kalledria")
                 
