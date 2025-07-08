1b9c416043fc0f61342fcd6742b30ed6d5c99592
RhoxGSAINT <swordknight@naver.com>
2025-07-09 00:05:31 +0900

    DLC lock
    Now the Nagash and Kalledria's DLC lock uses the system from Mixer. Thanks Mixu!

--- script/frontend/mod/rhox_nagash_kalledria.lua
+++ script/frontend/mod/rhox_nagash_kalledria.lua
@@ -2,14 +2,18 @@ core:add_ui_created_callback(
     function(context)
         if vfs.exists("script/frontend/mod/mixer_frontend.lua")then
     
-            if common.get_context_value("CcoOwnershipProductRecord", "TW_WH3_SHADOWS_OF_CHANGE", "IsOwned") or common.get_context_value("CcoOwnershipProductRecord", "TW_WH3_SHADOWS_OF_CHANGE_KSL", "IsOwned") then
-                mixer_enable_custom_faction("1568726704")
-                mixer_add_starting_unit_list_for_faction("mixer_vmp_wailing_conclave", {"wh2_dlc11_cst_inf_syreens","wh_main_vmp_inf_crypt_ghouls","wh_main_vmp_cav_hexwraiths", "wh_main_vmp_inf_cairn_wraiths"})
-                
-                mixer_change_lord_name("1568726704", "nag_vmp_kalledria")
-                
-                mixer_add_faction_to_major_faction_list("mixer_vmp_wailing_conclave")
-            end;
+            if common.get_context_value("CcoOwnershipProductRecord", "TW_WH1_BASE_GAME", "IsOwned") == false then
+                mixer_add_dlc_requirement_to_faction("mixer_vmp_wailing_conclave", "TW_WH1_BASE_GAME")
+            elseif common.get_context_value("CcoOwnershipProductRecord", "TW_WH3_SHADOWS_OF_CHANGE", "IsOwned") == false and common.get_context_value("CcoOwnershipProductRecord", "TW_WH3_SHADOWS_OF_CHANGE_KSL", "IsOwned") == false then
+                mixer_add_dlc_requirement_to_faction("mixer_vmp_wailing_conclave", "TW_WH3_SHADOWS_OF_CHANGE")
+            end   
+            mixer_enable_custom_faction("1568726704")
+            mixer_add_starting_unit_list_for_faction("mixer_vmp_wailing_conclave", {"wh2_dlc11_cst_inf_syreens","wh_main_vmp_inf_crypt_ghouls","wh_main_vmp_cav_hexwraiths", "wh_main_vmp_inf_cairn_wraiths"})
+            
+            mixer_change_lord_name("1568726704", "nag_vmp_kalledria")
+            
+            mixer_add_faction_to_major_faction_list("mixer_vmp_wailing_conclave")
+            
         end        
     end
 )
--- script/frontend/mod/rhox_nagash_mixer.lua
+++ script/frontend/mod/rhox_nagash_mixer.lua
@@ -1,49 +1,28 @@
 core:add_ui_created_callback(
-	function(context)
-	
-        if common.get_context_value("CcoOwnershipProductRecord", "TW_WH2_DLC11_VAMPIRE_COAST", "IsOwned") and common.get_context_value("CcoOwnershipProductRecord", "TW_WH2_TOMB_KINGS", "IsOwned") and common.get_context_value("CcoOwnershipProductRecord", "TW_WH1_LORDS_AND_UNITS_1", "IsOwned") then
-            if vfs.exists("script/frontend/mod/mixer_frontend.lua")then
-                mixer_enable_custom_faction("807711235")
-                mixer_change_lord_name("807711235", "nag_nagash_husk")
-                ----------TOW
-                mixer_enable_custom_faction("541325225")
-                mixer_change_lord_name("541325225", "nag_nagash_husk")
+    function(context)
 
-                mixer_add_starting_unit_list_for_faction("mixer_nag_nagash", {"nag_bone_golems","nag_nagashi_guard","nag_skeleton_reaper", "nag_vanilla_vmp_mon_fell_bats","nag_vanilla_tmb_inf_skeleton_warriors_0","nag_vanilla_tmb_inf_skeleton_spearmen_0", "nag_bone_thrower"})
-                
-                
-                mixer_add_faction_to_major_faction_list("mixer_nag_nagash")
-            end		
+        if vfs.exists("script/frontend/mod/mixer_frontend.lua")then
         
-        else
-            core:add_listener(
-                "rhox_nagash_hide_the_UI",
-                "ComponentLClickUp",
-                function (context)
-                    return context.string == "button_select_race"
-                end,
-                function (context)
-                    --out("Rhox Test: button clicked: ".. context.string)
-                    local parent = find_uicomponent(core:get_ui_root(), "popup_menu", "culture_list");
-                    if not parent then
-                        out("Rhox Nagash: Parent wasn't there")
-                        return
-                    end
-                    
-                
-                    local culture_to_kill = find_uicomponent(parent, "CcoCultureRecordmixer_nag_nagash");
-                    
-                    if not culture_to_kill then
-                        out("Rhox Nagash: There wasn't subculture to kill")
-                    end
-                    if culture_to_kill then
-                        out("Rhox Nagash: Killing UI")
-                        culture_to_kill:SetVisible(false)
-                    end
-                end,
-                true)
-		end
-	
-		
-	end
+            if common.get_context_value("CcoOwnershipProductRecord", "TW_WH2_DLC11_VAMPIRE_COAST", "IsOwned") == false then
+                mixer_add_dlc_requirement_to_faction("mixer_nag_nagash", "TW_WH2_DLC11_VAMPIRE_COAST")
+            elseif common.get_context_value("CcoOwnershipProductRecord", "TW_WH2_TOMB_KINGS", "IsOwned") == false then
+                mixer_add_dlc_requirement_to_faction("mixer_nag_nagash", "TW_WH2_TOMB_KINGS")
+            elseif common.get_context_value("CcoOwnershipProductRecord", "TW_WH1_LORDS_AND_UNITS_1", "IsOwned") == false then
+                mixer_add_dlc_requirement_to_faction("mixer_nag_nagash", "TW_WH1_LORDS_AND_UNITS_1")
+            end    
+            
+            mixer_enable_custom_faction("807711235")
+            mixer_change_lord_name("807711235", "nag_nagash_husk")
+            ----------TOW
+            mixer_enable_custom_faction("541325225")
+            mixer_change_lord_name("541325225", "nag_nagash_husk")
+
+            mixer_add_starting_unit_list_for_faction("mixer_nag_nagash", {"nag_bone_golems","nag_nagashi_guard","nag_skeleton_reaper", "nag_vanilla_vmp_mon_fell_bats","nag_vanilla_tmb_inf_skeleton_warriors_0","nag_vanilla_tmb_inf_skeleton_spearmen_0", "nag_bone_thrower"})
+            
+            
+            mixer_add_faction_to_major_faction_list("mixer_nag_nagash")
+        end
+
+
+    end
 )
