8ab8f68d3f8cea60bd987bfda4d21f090536f38c
RhoxGSAINT <swordknight@naver.com>
2025-10-01 11:21:05 +0900

    Scrap upgade color
    - Added a script so that Kalledria's scrap upgrade cost would show proper color depending on the player's pooled resource amount

--- script/campaign/mod/rhox_kalledria_scrap_upgrade.lua
+++ script/campaign/mod/rhox_kalledria_scrap_upgrade.lua
@@ -1,6 +1,34 @@
 local kalledria_faction = "mixer_vmp_wailing_conclave"
 
 
+local function rhox_loop_and_change_cost_text_color()
+    local effects_parent = find_uicomponent(core:get_ui_root(), "units_panel_scrap_upgrades", "scrap_upgrades_parent", "list_box");
+    if effects_parent then
+        local childCount = effects_parent:ChildCount()
+        for i=1, childCount - 1  do
+            local child = UIComponent(effects_parent:Find(i))
+            if not child then
+                return
+            end
+            local scrap_component = find_uicomponent(child, "scrap_text")
+            if not scrap_component then
+                return
+            end
+            local scarap_cost_text= scrap_component:GetStateText()
+            --out("Rhox Octopus check: "..scarap_cost_text)
+            local scrap_cost = tonumber(scarap_cost_text)
+            
+            
+            local pooled_resource_amount = cm:get_local_faction(true):pooled_resource_manager():resource("wh3_dlc24_ksl_spirit_essence"):value()
+            
+            if pooled_resource_amount >= scrap_cost then
+                scrap_component:SetState("positive")
+            end
+        end
+    end
+end
+
+
 cm:add_first_tick_callback(
     function()
         if cm:get_local_faction_name(true) == kalledria_faction then
@@ -48,13 +76,10 @@ cm:add_first_tick_callback(
             )
 
             core:add_listener(
-                'rhox_nagash_akkorak_clicked_upgrade_button',
+                'rhox_nagash_clicked_button_purchase_unit_upgrades',
                 'ComponentLClickUp',
                 function(context)
-                    
-
                     return context.string == "button_purchase_unit_upgrades" 
-                    
                 end,
                 function(context)
                     cm:callback(
@@ -64,10 +89,44 @@ cm:add_first_tick_callback(
                                 upgrade_button:SetVisible(true)
                                 upgrade_button:SetState("active")
                             end
+                            rhox_loop_and_change_cost_text_color()
                         end,
                         0.1
                     )
-                    
+                end,
+                true
+            )
+            
+            core:add_listener(
+                'rhox_nagash_clicked_upgrade_button',
+                'ComponentLClickUp',
+                function(context)
+                    return context.string == "button_upgrade" 
+                end,
+                function(context)
+                    cm:callback(
+                        function()
+                            rhox_loop_and_change_cost_text_color()
+                        end,
+                        0.1
+                    )
+                end,
+                true
+            )
+            
+            core:add_listener(
+                'rhox_nagash_clicked_land_unit',
+                'ComponentLClickUp',
+                function(context)
+                    return string.find(context.string, "LandUnit")
+                end,
+                function(context)
+                    cm:callback(
+                        function()
+                            rhox_loop_and_change_cost_text_color()
+                        end,
+                        0.1
+                    )
                 end,
                 true
             )
